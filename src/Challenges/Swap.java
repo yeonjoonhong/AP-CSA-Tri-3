449a4b1743942aea741d00990b9346cef0eb83e6
D1935827V <54718367+D1935827V@users.noreply.github.com>
2022-04-09 09:01:03 -0800

    Update Swap.java

--- src/Challenges/Swap.java
+++ src/Challenges/Swap.java
@@ -12,15 +12,30 @@ public class Swap extends Blueprint {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
 
-            System.out.println("BEFORE: a is: " + a + " b is: " + b);
-            int temp = b;
-            b = a;
-            a = temp;
-            System.out.println("AFTER: a is: " + a + " b is: " + b);
+            IntByReference.swapper(21, 16);
         } catch (Exception e) {
             System.out.println("Invalid input, try again");
             run();
         }
-
+        public static void swapper(int n0, int n1) {
+        IntByReference a = new IntByReference(n0);
+        IntByReference b = new IntByReference(n1);
+        System.out.println("Before: " + a + " " + b);
+        swapToLowHighOrder(a, b);  // conditionally build swap method to change values of a, b
+        System.out.println("After: " + a + " " + b);
+        System.out.println();
+        }
+        public static void swapToLowHighOrder(IntByReference a,IntByReference b) {
+        int num1 = a.value;
+        int num2 = b.value;
+        if (num2>num1) {
+        a.value = num2;
+        b.value = num1;
+        }
+        public IntByReference(int n) {
+        this.value = n;
+        }
+        public String toString(){//overriding the toString() method
+        return ""+value;
+        }
     }
-}
